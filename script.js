3066c7152aa1b0a8dec013f40473ee1aab417487
jessicalima22 <jessica.lima22@hotmail.com>
2023-09-11 23:46:06 -0300

    workout form validation (start)

--- script.js
+++ script.js
@@ -11,6 +11,50 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance; //in km
+    this.duration = duration; //in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    //km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+const run1 = new Running([39, 12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, 12], 27, 95, 523);
+console.log(run1, cycling1);
+
+////////////////////////////////////////////////////////////////////////
+//APPLICATION
 class App {
   #map;
   #mapEvent;
@@ -68,6 +112,24 @@ class App {
   }
   _newWorkout(e) {
     e.preventDefault();
+
+    //get data from form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duraetion = +inputDuration.value;
+
+    //if workout running, create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      //check if data is valid
+      if (!Number.isFinite(distance))
+        return alert('Inputs have to be positive numbers!');
+    }
+
+    //if workout cycling, create cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+    }
 
     //clear input fields
     inputCadence.value =

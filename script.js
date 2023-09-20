b2ff52623ecc97921ad6cc0a9165e0fc07f1e6e4
jessicalima22 <jessica.lima22@hotmail.com>
2023-09-20 03:03:57 -0300

    popup config

--- script.js
+++ script.js
@@ -73,10 +73,9 @@ class App {
   #workouts = [];
   constructor() {
     this._getPosition();
-
     form.addEventListener('submit', this._newWorkout.bind(this));
-
     inputType.addEventListener('change', this._toggleElevationField);
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
   _getPosition() {
     if (navigator.geolocation) {
@@ -251,6 +250,18 @@ class App {
 </li>`;
 
     form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest('.workout');
+    console.log(workoutEl);
+
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+    console.log(workout);
   }
 }
 
